6e25fe43cd2dbb5c382a2ce48b5c437aec23ff65
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-01-15 11:13:19 +0530

    Update events.py

--- fridaybot/events.py
+++ fridaybot/events.py
@@ -86,7 +86,7 @@ def register(**args):
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
                     text = "**USERBOT ERROR REPORT**\n"
-                    link = "[PaperplaneExtended Support Chat](https://t.me/deviluserbot)"
+                    link = "[PaperplaneExtended Support Chat](https://t.me/slayer_userbot_support)"
  
                     text += "If you want to, you can report it"
                     text += f"- just forward this message to {link}.\n"
